4a0520ea6dad791c6ba328701e180873a5f79cd3
Aiden Neale <an12@sanger.ac.uk>
2020-10-21 13:58:11 +0100

    Changed method of communication for OpenStack to novaclient

--- backend/report.py
+++ backend/report.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 """
-A module consisting of utility functions. 
+A module consisting of utility functions.
 """
-import openstack 
+from novaclient import client
+import openstack
 import time as x
 import random
 import os
@@ -19,7 +20,15 @@ import asyncio
 from datetime import datetime
 import dateutil.parser
 
-#Report class with the following functions: 
+#Nova Environment variables
+version = "2"
+username = os.getenv('OS_USERNAME')
+password = os.getenv('OS_PASSWORD')
+project_id = os.getenv('OS_PROJECT_ID')
+auth_url = os.getenv('OS_AUTH_URL')
+user_domain_name = os.getenv('OS_USER_DOMAIN_NAME')
+
+#Report class with the following functions:
 class Report:
     '''This class describes a report'''
 
@@ -38,14 +47,14 @@ class Report:
 async def update_report(report):
     """report = {user, server_name, cores, cpu hours, master, slaves}
     """
-   
+
     temp_cluster_list = await load_server_list()
     await load_cpu_time(temp_cluster_list)
     time = datetime.now()
     print('Time started')
     report.set_time(time)
     report.set_clusters(temp_cluster_list)
-  
+
     print('Time taken: '.format(datetime.now() - time))
 
     await asyncio.sleep(1000)
@@ -56,42 +65,42 @@ async def load_server_list():
         Establishes a connection by authenticating with keystone. (blocking)
         Gets all flavours (blocking).
         Creates a dictionary of users.
-        Iterates over the list of servers. Each server is mapped to some user and manipulates the user record. 
+        Iterates over the list of servers. Each server is mapped to some user and manipulates the user record.
     """
 
-    connection = openstack.connect()
+    nova = client.Client(version=version, username=username, password=password, project_id=project_id, auth_url=auth_url, user_domain_name=user_domain_name)
     # What if this fails?
-    flavor_id_map =  get_flavors(connection)
+    flavor_id_map =  get_flavors(nova)
     stored_users = {}
-    for server in connection.compute.servers():
-        user = server.get('metadata', {}).get('deployment_owner', None)
-        flavour_id = server.get('flavor', {}).get('id')
-        vcpus = flavor_id_map.get(flavour_id)
-        is_master = server.get('metadata', {}).get('role_name', None) == 'hail-master'
+    for server in nova.servers.list():
+        user = server.metadata.get('deployment_owner', None)
+        flavor_id = server.flavor.get('id')
+        vcpus = flavor_id_map.get(flavor_id)
+        is_master = server.metadata.get('role_name', None) == 'hail-master'
         user_data = stored_users.get(user)
         if user_data is None:
-            user_data = {"user_name": user, "server_names": [server['name']]}
+            user_data = {"user_name": user, "server_names": [server.name]}
             user_data['cores'] = vcpus
             user_data['cpu_hours'] = None
             if (is_master):
-                created_date = server.get('created_at', None)
+                created_date = server.created
                 user_data['created_date'] = dateutil.parser.parse(created_date).strftime("%d-%m-%Y %H:%M:%S")
                 user_data['slaves'] = 0
                 user_data['master'] = 1
             else:
-                user_data['slaves'] = 1 
+                user_data['slaves'] = 1
                 user_data['master'] = 0
-                created_date = server.get('created_at', None)
+                created_date = server.created
                 # user_data['created_date'] = datetime.strptime(created_date, '%Y-%m-%dT%H:%M:%S%Z').strftime("%d-%m-%Y %H:%M:%S")
                 user_data['created_date'] = dateutil.parser.parse(created_date).strftime("%d-%m-%Y %H:%M:%S")
             stored_users[user] = user_data
         else:
-            user_data['server_names'].append(server['name'])
+            user_data['server_names'].append(server.name)
             user_data['cores'] = user_data['cores'] + vcpus
             if not is_master:
                 user_data['slaves'] = user_data['slaves'] + 1
             else:
-                created_date = server.get('created_at', None)
+                created_date = server.created
                 # user_data['created_date'] = datetime.strptime(created_date, '%Y-%m-%dT%H:%M:%S%z').strftime("%d-%m-%Y %H:%M:%S")
                 user_data['created_date'] = dateutil.parser.parse(created_date).strftime("%d-%m-%Y %H:%M:%S")
                 user_data['master'] = user_data['master'] + 1
@@ -107,6 +116,7 @@ async def load_cpu_time(cluster_list):
         except Exception as e:
             print("Error while loading cpu time for user: ", cluster['user_name'])
 
+'''
 async def load_cpu_time(cluster_list):
     for cluster in cluster_list:
         cluster['cpu_hours'] = calculate_cpu_time_for_cluster(cluster)
@@ -114,28 +124,27 @@ async def load_cpu_time(cluster_list):
         asyncio.gather(*[cluster['cpu_hours'] for cluster in cluster_list])
     except Exception as e:
         print("Error while loading cpu time for user: ", cluster['user_name'])
+'''
 
 
-
-def get_flavors(connection):
-    flavors = connection.list_flavors(connection)
+def get_flavors(nova):
+    flavors=nova.flavors.list(detailed=True)
     flavorIdMap = {}
+
     for flavor in flavors:
-        id = flavor.get('id', None)
+        id = flavor.id
         if id is not None:
-            flavorIdMap[id] = flavor.get('vcpus', None)
+            flavorIdMap[id] = flavor.vcpus
     print(flavorIdMap)
     return flavorIdMap
 
 
-        
 
 async def calculate_cpu_time_for_cluster(cluster):
     cpu_hours = 0
     server_names = cluster['server_names']
     try:
-        latest_cpu_hours = asyncio.gather(*[get_cpu_time(server_name) for server_name in server_names])
-        await latest_cpu_hours
+        latest_cpu_hours = await asyncio.gather(*[get_cpu_time(server_name) for server_name in server_names])
         cpu_hours = sum(latest_cpu_hours)
     except Exception:
         raise e
@@ -146,31 +155,18 @@ async def calculate_cpu_time_for_cluster(cluster):
 
 async def get_cpu_time(server_name):
     try:
-        bashCommand = "openstack server show {} --diagnostics -f json".format(server_name)
-        print(bashCommand)
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        print("ërror ", error)
-        print("output ", output)
-        if output is not None:
-            output_json_string = output.decode('utf8').replace("\n", "")
-            output_dictionary= json.loads(output_json_string)
-            cpu_time = 0;
-        
-            for key, value in output_dictionary.items():
+        nova = client.Client(version=version, username=username, password=password, project_id=project_id, auth_url=auth_url, user_domain_name=user_domain_name)
+        server = nova.servers.list(search_opts={'name': server_name})
+        if server is not None:
+            process = server[0].diagnostics()
+            output = process[1]
+            print(output)
+            cpu_time = 0
+            for key, value in output.items():
                 if key.startswith("cpu"):
-                    print(key, ":" , value) 
+                    print(key, ":", value)
                     cpu_time += value
             return round(cpu_time/(3600*1000000000))
     except Exception as e:
         print("Could not calculate cputime for:", server_names)
-        raise e             
-
-
-
-    
-
-
-
-
-
+        raise e
